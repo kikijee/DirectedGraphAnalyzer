d9d8d3abf875307b4b3a7dca74f386a84ea72e15
kikijee <cam007mani@gmail.com>
2022-01-04 18:50:31 -0800

    comment(s)

--- ShortestPathClient.cpp
+++ ShortestPathClient.cpp
@@ -2,15 +2,15 @@
 #include <fstream>
 #include <stdlib.h>
 #include "ShortestPath.h"
-
+// client purpose to provide main menu and give analysis options to user based on table selected to use as graph
+// current analysis options: Adjacency list analysis, dikstra's analysis, and depth first search
 int main(){
     std::string fname = "No file selected";
-    std::string file;
-    bool fileSelect = false;
+    std::string file;    
     char choice;
     int num;
     int case_num;
-    Area* a_ptr = nullptr;
+    Area* a_ptr = nullptr;      // when program first runs there is no defualt file selected, user must first select file
     while(true){
         std::cout<<"...WELCOME TO THE DIRECTED GRAPH ANALYZER..."<<std::endl;
         std::cout<<"Please select from the options below"<<std::endl;
@@ -19,7 +19,7 @@ int main(){
         std::cout<<"=> ";
         std::cin >> case_num;
         switch(case_num){
-            case 1:
+            case 1:     // file select
                 while(true){
                     std::cout<<"Please enter the file name that you would like to process, other wise type '/' to exit"<<std::endl;
                     std::cout<<"=> ";
@@ -31,7 +31,7 @@ int main(){
                         std::cin >> choice;
                         if(choice == 'y' || choice == 'Y'){
                             fname = file;
-                            if(a_ptr != nullptr){delete a_ptr;}
+                            if(a_ptr != nullptr){delete a_ptr;} // if there was already a previous file selected, delete current area object and have the pointer point to a new one
                             fin>>num;
                             a_ptr = new Area(num);
                             a_ptr->read_table(fin);
@@ -46,7 +46,7 @@ int main(){
                 }
                 system("CLS");
                 break;
-            case 2:
+            case 2:     // Adjacency table analysis
                 if(a_ptr == nullptr){std::cout<<"...Please select file to read from first..."<<std::endl;break;}
                 std::cout<<"ADJACENCY TABLE:"<<std::endl;
                 a_ptr->display_table();
@@ -64,7 +64,7 @@ int main(){
                 }
                 system("CLS");
                 break;
-            case 3:
+            case 3:     // Dikstra's Algorithm
                 if(a_ptr == nullptr){std::cout<<"...Please select file to read from first..."<<std::endl;break;}
                 while(true){
                     std::cout<<"please enter vertex value to create shortest path table or type '/' to exit:"<<std::endl;
@@ -90,7 +90,7 @@ int main(){
                 }
                 system("CLS");
                 break;
-            case 4:
+            case 4:     // Depth First search
                 if(a_ptr == nullptr){std::cout<<"...Please select file to read from first..."<<std::endl;break;}
                 a_ptr->display_table();
                 AdjacentNode *n_ptr;
@@ -134,7 +134,7 @@ int main(){
                 a_ptr->clear_visit_numbers();
                 system("CLS");
                 break;
-            case 5:
+            case 5:     // Exit option
                 system("CLS");
                 break;
             default:
